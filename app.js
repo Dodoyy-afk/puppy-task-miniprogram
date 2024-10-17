0d6232f2fef4c3e9e465e3f3c556651969e224e2
Dodoyy-afk <Yingjian.Huang16@gmail.com>
2024-10-17 01:46:02 -0700

    set name to global variable

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-// app.js
-App({})
+App({
+  async onLaunch() {
+    // init cloud environment
+    // this.initcloud()
+
+    this.global_data = {
+      _puppy_white_id: "",
+      _puppy_yellow_id: "",
+
+      puppy_white: "小白",
+      puppy_yellow: "小鸡毛",
+
+      credit_limit: 50
+    }
+  }
+})
