4196e4fb5b91a4c5ecedb1b903261d1d8eba9dfa
Dodoyy-afk <Yingjian.Huang16@gmail.com>
2024-10-17 20:48:48 -0700

    title debug

--- pages/task/index.js
+++ pages/task/index.js
@@ -3,8 +3,8 @@ Page({
         _puppy_white_id: getApp().global_data._puppy_white_id,
         _puppy_yellow_id: getApp().global_data._puppy_yellow_id,
 
-        task_list: [1],
-        task_pending: [],
+        task_list: [],
+        task_pending: [2],
         task_done: [],
 
         searching_content: ""
